bb25573bd48416b804118d8e451fca5977d77cc8
Phillis Yuan <tigglywuff@gmail.com>
2018-01-09 22:23:17 -0500

    cleaned up requestImages.js

--- requestImages.js
+++ requestImages.js
@@ -1,7 +1,6 @@
 const API_KEY = "9e51e7c9bb20854422c5c447702215bf";
 
 function generateFlickrUrl(tag) {
-    const baseUrl = "https://api.flickr.com/services/rest/";
     var queryParams = {
         "method": "flickr.photos.search",
         "api_key": API_KEY,
@@ -10,25 +9,16 @@ function generateFlickrUrl(tag) {
         "nojsoncallback": 1
     };
 
-    return baseUrl + this.jsonToQueryParams(queryParams);
+    return "https://api.flickr.com/services/rest/" + this.jsonToQueryParams(queryParams);
 }
 
 function jsonToQueryParams(json) {
-    var jsonKeys = [],
-        retStr = "";
+    var retStr = "";
 
     json = json || {};
-    jsonKeys = Object.keys(json);
 
-    // json is empty
-    if (!jsonKeys.length) {
-        return "";
-    }
-
-    jsonKeys.forEach(function (k) {
-        var v = json[k];
-
-        retStr += "&" + k + "=" + v;
+    Object.keys(json).forEach(function (k) {
+        retStr += "&" + k + "=" + json[k];;
     });
 
     // replace leading & with a ?
@@ -52,10 +42,7 @@ function getAsync(url, cb) {
   * @param photo {object}
   */
 function photoToUrl(photo) {
-    var url = "https://farm";
-
-    url += photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
-    return url;
+    return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
 }
 
 function main() {
